3c934224debfceef1fd9bf6c70ce0d454949bc18
misl <michal.slovik@tutanota.com>
2024-02-16 15:42:46 +0100

    update

---
+++ src/main/java/arrays/Sums.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.List;
+
+public class Sums {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}
