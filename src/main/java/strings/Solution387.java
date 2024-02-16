9507df1f35d231d23602624f6c8ee73b8881b8a1
misl <michal.slovik@tutanota.com>
2024-02-16 22:23:44 +0100

    Add

---
+++ src/main/java/strings/Solution387.java
@@ -0,0 +1,29 @@
+package strings;
+
+import java.util.HashMap;
+
+public class Solution387 {
+    public int firstUniqChar(String s) {
+        // Given a string s,
+        // find the first non-repeating character in it and
+        // return its index.
+        // If it does not exist, return -1.
+        HashMap<Character, Integer> repeatingStr = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+
+            repeatingStr.put(s.charAt(i), repeatingStr.getOrDefault(s.charAt(i), 0) + 1);
+
+        }
+        System.out.println(repeatingStr);
+
+
+        for (int i = 0; i < s.length(); i++) {
+            var currentChar = s.charAt(i);
+            int valueAt = repeatingStr.getOrDefault(currentChar, 0);
+            if (valueAt == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/strings/Solution387Test.java
@@ -0,0 +1,23 @@
+package strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution387Test {
+
+    @Test
+    void testFirstSample() {
+        assertEquals(0, new Solution387().firstUniqChar("leetcode"));
+    }
+
+    @Test
+    void testSecondSample() {
+        assertEquals(2, new Solution387().firstUniqChar("loveleetcode"));
+    }
+
+    @Test
+    void testThirdSample() {
+        assertEquals(-1, new Solution387().firstUniqChar("aabb"));
+    }
+}
