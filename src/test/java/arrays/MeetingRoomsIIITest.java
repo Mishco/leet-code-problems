ca57e7088d6a19f21b57402015db107954724a94
misl <michal.slovik@tutanota.com>
2024-02-19 14:19:03 +0100

    Updates

---
+++ src/test/java/arrays/MeetingRoomsIIITest.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetingRoomsIIITest {
+
+    @Test
+    void simpleTest() {
+        var meetings = new int[][]{{1, 5}, {0, 10}, {2, 7}, {3, 4}};
+        assertEquals(0, new MeetingRoomsIII().mostBooked(2, meetings));
+    }
+
+    @Test
+    void secondTest() {
+        var meetings = new int[][]{{1, 20}, {2, 10}, {3, 5}, {4, 9}, {6, 8}};
+        assertEquals(1, new MeetingRoomsIII().mostBooked(3, meetings));
+    }
+
+}
