c01b1917cf42545f8f126cdc521cece186d1ff2a
misl <michal.slovik@tutanota.com>
2024-02-21 09:31:27 +0100

    Add RomanToInteger

---
+++ src/main/java/easy/RomanToInteger.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Map<Character, Integer> symbols = new HashMap<>();
+        symbols.put('I', 1);
+        symbols.put('V', 5);
+        symbols.put('X', 10);
+        symbols.put('L', 50);
+        symbols.put('C', 100);
+        symbols.put('D', 500);
+        symbols.put('M', 1000);
+
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = symbols.get(s.charAt(i));
+            if (i < s.length() - 1 && value < symbols.get(s.charAt(i + 1))) {
+                total -= value;
+            } else {
+                total += value;
+            }
+        }
+
+        return total;
+    }
+
+}
---
+++ src/test/java/easy/RomanToIntegerTest.java
@@ -0,0 +1,24 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanToIntegerTest {
+
+    @Test
+    void testSimple() {
+        assertEquals(3, new RomanToInteger().romanToInt("III"));
+    }
+
+    @Test
+    void testSecondSimple() {
+        assertEquals(58, new RomanToInteger().romanToInt("LVIII"));
+    }
+
+    @Test
+    void longerTest() {
+        assertEquals(1994, new RomanToInteger().romanToInt("MCMXCIV"));
+    }
+
+}
