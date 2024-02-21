cff48c8e1e6ff041c7abe915c0768eeba51db4f4
misl <michal.slovik@tutanota.com>
2024-02-21 06:30:54 +0100

    Add today challenge

---
+++ src/main/java/bitwise/Solution201.java
@@ -0,0 +1,20 @@
+package bitwise;
+
+public class Solution201 {
+    public int rangeBitwiseAnd(int left, int right) {
+        // Given two integers left and right that represent the range [left, right],
+        // return the bitwise AND of all numbers in this range, inclusive.
+
+        // if m !==n that means one of the bits is 0 so keep right shifting
+        // until all the bits in both are same  and when they are same number of
+        // shift will be equal to number of bits not same just right shift it then
+
+        int s = 0;
+        while (left != right) {
+            left = left >> 1;
+            right = right >> 1;
+            s++;
+        }
+        return (left << s);
+    }
+}
---
+++ src/test/java/bitwise/Solution201Test.java
@@ -0,0 +1,19 @@
+package bitwise;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution201Test {
+
+    @ParameterizedTest
+    @CsvSource({
+            "5, 7, 4",
+            "0, 0, 0",
+            "1, 2147483647, 0"
+    })
+    void testRangeBitwiseAnd(int m, int n, int expected) {
+        assertEquals(expected, new Solution201().rangeBitwiseAnd(m, n));
+    }
+}
