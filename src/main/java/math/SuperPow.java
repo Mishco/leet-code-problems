fa3e1ac2dd570d40d5a14ea868f8c00ad340c5c5
misl <michal.slovik@tutanota.com>
2024-03-06 12:55:04 +0100

    Updates

---
+++ src/main/java/math/SuperPow.java
@@ -0,0 +1,36 @@
+package math;
+
+public class SuperPow {
+
+    private int powMod(int a, int b, int mod) {
+        int result = 1;
+        a %= mod;
+
+        while (b > 0) {
+            if (b % 2 == 1) {
+                result = (result * a) % mod;
+            }
+
+            a = (a * a) % mod;
+            b /= 2;
+        }
+
+        return result;
+    }
+
+    public int superPow(int a, int[] b) {
+        if (a == 1) {
+            return 1;
+        }
+
+        int result = 1;
+        int mod = 1337;
+
+        for (int i = b.length - 1; i >= 0; i--) {
+            int digit = b[i];
+            result = (result * powMod(a, digit, mod)) % mod;
+            a = powMod(a, 10, mod);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/math/SuperPowTest.java
@@ -0,0 +1,31 @@
+package math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SuperPowTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(8, new SuperPow().superPow(2, new int[]{3}));
+    }
+
+    @Test
+    void secondTest() {
+        assertEquals(1024, new SuperPow().superPow(2, new int[]{1, 0}));
+    }
+
+    @Test
+    void thirdTest() {
+        assertEquals(1, new SuperPow().superPow(1, new int[]{4, 3, 3, 8, 5, 2}));
+    }
+
+    @Test
+    void fourthTest() {
+        int actual = new SuperPow().superPow(2147483647, new int[]{2, 0, 0});
+        int expected = 1198;
+        assertEquals(expected, actual);
+    }
+
+}
