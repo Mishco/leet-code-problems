5a031e20c4558a75e9ebe9299c5b1c6603074e9d
misl <michal.slovik@tutanota.com>
2024-03-06 10:18:56 +0100

    Update MySqrt

---
+++ src/test/java/math/MySqrtTest.java
@@ -0,0 +1,26 @@
+package math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySqrtTest {
+
+    @Test
+    void oneSample() {
+        assertEquals(2,new MySqrt().mySqrt(4));
+    }
+
+
+    @Test
+    void twoSample() {
+        assertEquals(0,new MySqrt().mySqrt(0));
+    }
+
+    @Test
+    void thirdSample() {
+        assertEquals(2,new MySqrt().mySqrt(8));
+    }
+
+
+}
