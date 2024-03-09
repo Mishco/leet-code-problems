a888b8e550fdace729f02cfcd58b92e03fa8e655
misl <michal.slovik@tutanota.com>
2024-03-09 21:40:06 +0100

    updates

---
+++ src/main/java/arrays/MinimumCommonValue.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+
+        int i = 0;
+        int j = 0;
+        int common = -1;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                common = nums1[i];
+                break;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return common;
+
+    }
+}
---
+++ src/test/java/arrays/MinimumCommonValueTest.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumCommonValueTest {
+
+    @Test
+    void testOne() {
+        assertEquals(2,new MinimumCommonValue().getCommon(new int[]{1,2,3}, new int[]{2,4}));
+    }
+    @Test
+    void testTwo() {
+        assertEquals(2,new MinimumCommonValue().getCommon(new int[]{1,2,3,6}, new int[]{2,3,4,5}));
+    }
+
+}
