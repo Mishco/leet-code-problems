310f0c74d923331713ee29698e9cf8a941b1d217
misl <michal.slovik@tutanota.com>
2024-03-11 09:42:35 +0100

    Cleaning

--- src/test/java/math/PascalTriangleIITest.java
+++ src/test/java/math/PascalTriangleIITest.java
@@ -1,19 +1,18 @@
 package math;
 
-import com.sun.jdi.ArrayReference;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PascalTriangleIITest {
 
     @Test
     void testOne() {
         List<Integer> row = new PascalTriangleII().getRow(3);
-        List<Integer> excepted = Arrays.asList(1,3,3,1);
+        List<Integer> excepted = Arrays.asList(1, 3, 3, 1);
         assertEquals(row, excepted);
     }
 
@@ -23,10 +22,11 @@ class PascalTriangleIITest {
         List<Integer> excepted = List.of(1);
         assertEquals(row, excepted);
     }
+
     @Test
     void testThird() {
         List<Integer> row = new PascalTriangleII().getRow(1);
-        List<Integer> excepted = Arrays.asList(1,1);
+        List<Integer> excepted = Arrays.asList(1, 1);
         assertEquals(row, excepted);
     }
 }
