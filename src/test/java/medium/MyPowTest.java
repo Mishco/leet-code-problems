df409e823edee0b3e69831f050812340c63511f6
misl <michal.slovik@tutanota.com>
2024-05-08 21:02:18 +0200

    Fix tests

--- src/test/java/medium/MyPowTest.java
+++ src/test/java/medium/MyPowTest.java
@@ -2,18 +2,21 @@ package medium;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyPowTest {
 
     @Test
     void oneExample() {
-        assertEquals(1024.00,new MyPow().myPow(2.000, 10));
+        assertEquals(1024.00, new MyPow().myPow(2.000, 10));
     }
 
     @Test
     void twoExample() {
-        assertEquals(9.26100,new MyPow().myPow(2.100, 3));
+        double value = new MyPow().myPow(2.100, 3);
+        value = Math.round(value * 1000.00);
+        value = value / 1000.00;
+        assertEquals(9.26100, value);
     }
 
 
@@ -24,7 +27,11 @@ class MyPowTest {
 
     @Test
     void failingTest() {
-        assertEquals(700.28148,new MyPow().myPow(8.88023, 3));
+        double value = new MyPow().myPow(8.88023, 3);
+        value = Math.round(value * 100000.00);
+        value = value / 100000.00;
+
+        assertEquals(700.28148, value);
     }
 
 }
